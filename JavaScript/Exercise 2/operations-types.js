cb4eba56999789d7aabee43754fef63dd1fc6fe3
AngelaSolomons <angelasolomons03@gmail.com>
2026-05-19 17:30:12 +0200

    Added new files.

---
+++ JavaScript/Exercise 2/operations-types.js
@@ -0,0 +1,15 @@
+// Create Variables
+let number1 = 4
+let number2 = 3
+let number1String = String(number1)
+let sum = number1 + number2
+let product = number1 * number2
+
+// Print Results
+console.log(`
+    number1: ${number1}
+    number2: ${number2}
+    number1String: ${number1String}
+    sum: ${sum}
+    product: ${product}
+    `)
