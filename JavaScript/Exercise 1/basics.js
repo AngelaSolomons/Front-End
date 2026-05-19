56e1d9f7169cc848d0eaf6f2b0a68f761b5d329b
AngelaSolomons <angelasolomons03@gmail.com>
2026-05-19 15:20:13 +0200

    Updated basics.js file

--- JavaScript/Exercise 1/basics.js
+++ JavaScript/Exercise 1/basics.js
@@ -1,11 +1,14 @@
+// Create variables
 const firstName = "Angela"
 const lastName = "Solomons"
 let age = 33
 let fullName = firstName +" "+  lastName
 
+// Print greeting
 console.log(`Welcome ${fullName}! I heard you are turning ${age}`)
 
 // Change age
 age = 23
 
+// Print changes
 console.log(`Welcome ${fullName}! I heard you are turning ${age}`)
